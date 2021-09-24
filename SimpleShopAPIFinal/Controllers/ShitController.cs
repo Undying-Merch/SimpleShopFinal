2c2397bc18a6837fc328e8b8ebfef584599c86ed
Undying-Merch <73592997+Undying-Merch@users.noreply.github.com>
2021-09-24 11:38:11 +0200

    Just fucking work

---
+++ SimpleShopAPIFinal/Controllers/ShitController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleShopAPIFinal.Controllers
+{
+    public class ShitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimpleShopModels/Temp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace SimpleShopModels
+{
+    class Temp
+    {
+        public int tempId { get; private set; }
+        public float TempC { get; set; }
+        public float TempF { get; set; }
+        public string lokation { get; set; }
+
+        [JsonConstructor]
+        public Temp(float tempC, float tempF, string location)
+        {
+            TempC = tempC;
+            TempF = tempF;
+            lokation = location;
+        }
+
+        public void SetId(int id)
+        {
+            tempId = id;
+        }
+    }
+}
